e607abdf0fd84a57941050745a615c94d67ca9d1
David <ariedov@gmail.com>
2014-06-15 00:25:13 +0300

    implemented change image on image click

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,9 @@
 window.onload = function() {
+
+	var bigImage = $("#image_container img");
+	var images = $(".album a img");
+	var currentIndex = 0;
+
 	$("#sculpture.art_elem a").click(function(ev) {
 		$("#gallery").fadeIn(1000);
 		return false;
@@ -9,14 +14,29 @@ window.onload = function() {
 		return false;
 	});
 
-	$(".album a img").click(function(ev) {
-		var bigImage = $("#image_container img");
+	images.click(function(ev) {
+		currentIndex = images.index(this);	
 		var imgSrc = $(this).attr("src");	
 		bigImage.fadeOut(400, function() {
 			bigImage.attr("src", imgSrc);
 			bigImage.fadeIn();
 			return false;
 		});
+
+		return false;
+	});
+
+	bigImage.click(function(ev) {
+		currentIndex += 1;
+		var currentImg = images[currentIndex];
+		if (currentImg != undefined) {
+			var imgSrc = $(currentImg).attr("src");
+			bigImage.fadeOut(400, function() {
+				bigImage.attr("src", imgSrc);
+				bigImage.fadeIn();
+				return false;
+			});
+		}
 		return false;
 	});
 }
