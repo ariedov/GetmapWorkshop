39e92d541a634d2990d3b343a1e46cad526f8496
david <ariedov@gmail.com>
2015-06-07 22:22:24 +0300

    styling links for gallery elements

--- js/gallery.js
+++ js/gallery.js
@@ -5,7 +5,10 @@ $(document).ready(function() {
 	var images = [];
 	var currentIndex = 0;
 
-	$("#image_caption").click(function() {
+	$("#image_caption p").click(function() {
+		return false;
+	}).children().click(function() {
+		window.open($(this).attr('href'), "_blank");
 		return false;
 	});
 
@@ -18,14 +21,16 @@ $(document).ready(function() {
 		}
 		images.show();
 		bigImage.attr("src", $(image).attr("data-full"));
-		imageCaption.text($(image).attr("data-caption"));
+		imageCaption.html($(image).attr("data-caption"));
 
 		images.click(function(ev) {
 			currentIndex = $(this).index();	
 			var imgSrc = $(this).attr("data-full");	
+			var imgCaption = $(this).attr("data-caption");
 			bigImage.fadeOut(400, function() {
 				bigImage.attr('src','data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAA‌​LAAAAAABAAEAAAICRAEAOw%3D%3D');
 				bigImage.attr("src", imgSrc);
+				imageCaption.html(imgCaption);
 				bigImage.fadeIn();
 				return false;
 			});
@@ -54,7 +59,7 @@ $(document).ready(function() {
 			bigImage.fadeOut(400, function() {
 				bigImage.attr('src','data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAA‌​LAAAAAABAAEAAAICRAEAOw%3D%3D');
 				bigImage.attr("src", imgSrc);
-				imageCaption.text(imgCaption);
+				imageCaption.html(imgCaption);
 				bigImage.fadeIn();
 				return false;
 			});
