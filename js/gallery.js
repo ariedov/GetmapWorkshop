ed36a9eb5b2d8ac0ec994841615bea37c45a5352
david <ariedov@gmail.com>
2015-06-12 00:35:18 +0300

    implemented colorbox

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,43 @@
 $(document).ready(function() {
-	$("#graphics").colorbox({rel: "ggraphics"});
-	$("#molding").colorbox({rel: "gmolding"});
-	$("#painting").colorbox({rel: "gpainting"});
-	$("#design").colorbox({rel: "design"});
-	$("#art").colorbox({rel: "gart"});
-	$("#sculpture").colorbox({rel: "gsculpture"});
-	$("#wpaint").colorbox({rel: "wpaint"});
+	var $graphics = $(".ggraphics").colorbox({rel: "ggraphics"});
+	$("#graphics a").click(function(e) {
+		e.preventDefault();
+		$graphics.eq(0).click();
+	});
+
+	var $molding = $(".gmolding").colorbox({rel: "gmolding"});
+	$("#molding a").click(function(e) {
+		e.preventDefault();
+		$molding.eq(0).click();
+	});
+
+	var $painting = $(".gpainting").colorbox({rel: "gpainting"});
+	$("#painting a").click(function(e) {
+		e.preventDefault();
+		$painting.eq(0).click();
+	});
+
+	var $design = $(".gdesign").colorbox({rel: "gdesign"});
+	$("#design a").click(function(e) {
+		e.preventDefault();
+		$design.eq(0).click();
+	});
+
+	var $art = $(".gart").colorbox({rel: "gart"});
+	$("#art a").click(function(e) {
+		e.preventDefault();
+		$art.eq(0).click();
+	});
+
+	$sculpture = $(".gsculpture").colorbox({rel: "gsculpture"});
+	$("#sculpture a").click(function(e) {
+		e.preventDefault();
+		$sculpture.eq(0).click();
+	});
+
+	$wpaint = $(".gwpaint").colorbox({rel: "gwpaint"});
+	$("#wpaint a").click(function(e) {
+		e.preventDefault();
+		$wpaint.eq(0).click();
+	});
 });
