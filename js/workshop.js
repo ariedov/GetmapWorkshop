287218ccfbd5509cda6891c368c81b5b2b703372
David <dleibovych@cogniance.com>
2015-11-10 11:48:09 +0200

    beautifying code

--- js/workshop.js
+++ js/workshop.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
-	var currentImageIndex = 0;
-	var imageCount = 6;
+  var currentImageIndex = 0;
+  var imageCount = 6;
 
-	var visibleImage = $(".workshop_image.visible");
-	var invisibleImage = $(".workshop_image.hidden");
+  var visibleImage = $(".workshop_image.visible");
+  var invisibleImage = $(".workshop_image.hidden");
 
-	var changePhoto = function() {
-		var nextImageIndex = (currentImageIndex + 1) % imageCount;
-		visibleImage.attr('src', "img/workshop/" + currentImageIndex + ".png");
-		invisibleImage.fadeOut(0, function() {
-			invisibleImage.attr('src', "img/workshop/" + nextImageIndex + ".png");
-			invisibleImage.fadeIn(3000);
-		});
+  var changePhoto = function() {
+    var nextImageIndex = (currentImageIndex + 1) % imageCount;
+    visibleImage.attr('src', "img/workshop/" + currentImageIndex + ".png");
+    invisibleImage.fadeOut(0, function() {
+      invisibleImage.attr('src', "img/workshop/" + nextImageIndex + ".png");
+      invisibleImage.fadeIn(3000);
+    });
 
-		currentImageIndex = nextImageIndex;
-	};
+    currentImageIndex = nextImageIndex;
+  };
 
-	setInterval(changePhoto, 4000);
+  setInterval(changePhoto, 4000);
 });
